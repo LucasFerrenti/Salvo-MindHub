3ee59ab4c4fed769e89f834761a0adb7c983427f
Lucas Ferrenti <LukasFerrenti@hotmail.com>
2021-10-28 18:24:58 -0300

    create project

---
+++ Salvo/Salvo/Models/SalvoContex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Salvo.Models
+{
+    public class SalvoContex : DbContext
+    {
+    }
+}
